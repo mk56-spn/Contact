dda773b1b6d4aa6e2815d82eb7072ddff18f8548
mk56-spn <darioheadleysilva@gmail.com>
2023-03-07 12:37:17 +0100

    Initial commit

---
+++ osu.Game.Rulesets.Contact/Replays/ContactAutoGenerator.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Game.Beatmaps;
+using osu.Game.Rulesets.Contact.Objects;
+using osu.Game.Rulesets.Replays;
+
+namespace osu.Game.Rulesets.Contact.Replays
+{
+    public class ContactAutoGenerator : AutoGenerator<ContactReplayFrame>
+    {
+        public new Beatmap<ContactHitObject> Beatmap => (Beatmap<ContactHitObject>)base.Beatmap;
+
+        public ContactAutoGenerator(IBeatmap beatmap)
+            : base(beatmap)
+        {
+        }
+
+        protected override void GenerateFrames()
+        {
+            Frames.Add(new ContactReplayFrame());
+
+            foreach (ContactHitObject hitObject in Beatmap.HitObjects)
+            {
+                Frames.Add(new ContactReplayFrame
+                {
+                    Time = hitObject.StartTime,
+                    Position = hitObject.Position,
+                    // todo: add required inputs and extra frames.
+                });
+            }
+        }
+    }
+}
---
+++ osu.Game.Rulesets.Contact/UI/DrawableContactRuleset.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Collections.Generic;
+using osu.Framework.Allocation;
+using osu.Framework.Input;
+using osu.Game.Beatmaps;
+using osu.Game.Input.Handlers;
+using osu.Game.Replays;
+using osu.Game.Rulesets.Contact.Objects;
+using osu.Game.Rulesets.Contact.Objects.Drawables;
+using osu.Game.Rulesets.Contact.Replays;
+using osu.Game.Rulesets.Mods;
+using osu.Game.Rulesets.Objects.Drawables;
+using osu.Game.Rulesets.UI;
+
+namespace osu.Game.Rulesets.Contact.UI
+{
+    [Cached]
+    public partial class DrawableContactRuleset : DrawableRuleset<ContactHitObject>
+    {
+        public DrawableContactRuleset(ContactRuleset ruleset, IBeatmap beatmap, IReadOnlyList<Mod> mods = null)
+            : base(ruleset, beatmap, mods)
+        {
+        }
+
+        protected override Playfield CreatePlayfield() => new ContactPlayfield();
+
+        protected override ReplayInputHandler CreateReplayInputHandler(Replay replay) => new ContactFramedReplayInputHandler(replay);
+
+        public override DrawableHitObject<ContactHitObject> CreateDrawableRepresentation(ContactHitObject h) => new DrawableContactHitObject(h);
+
+        protected override PassThroughInputManager CreateInputManager() => new ContactInputManager(Ruleset?.RulesetInfo);
+    }
+}
