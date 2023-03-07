a4b3e88c7c16b5c149e599256431080e7d51e925
mk56-spn <darioheadleysilva@gmail.com>
2023-03-07 13:08:28 +0100

    define playfield size and contents

--- osu.Game.Rulesets.Contact/UI/ContactPlayfield.cs
+++ osu.Game.Rulesets.Contact/UI/ContactPlayfield.cs
@@ -2,21 +2,23 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using osu.Framework.Allocation;
-using osu.Framework.Graphics;
 using osu.Game.Rulesets.UI;
+using osu.Game.Rulesets.Contact.Objects.Controller;
 
 namespace osu.Game.Rulesets.Contact.UI
 {
     [Cached]
     public partial class ContactPlayfield : Playfield
     {
+        public const int SIZE = 10000;
+
+        public ControllerArea ControllerArea { get; private set; }
+
         [BackgroundDependencyLoader]
         private void load()
         {
-            AddRangeInternal(new Drawable[]
-            {
-                HitObjectContainer,
-            });
+            ControllerArea = new ControllerArea();
+            AddInternal(ControllerArea);
         }
     }
 }
