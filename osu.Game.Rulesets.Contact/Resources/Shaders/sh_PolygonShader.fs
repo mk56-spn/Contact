2f1733bb01994259291a8260e391daae3897f337
mk56-spn <darioheadleysilva@gmail.com>
2023-03-10 09:52:41 +0100

    Add test shaders

--- osu.Game.Rulesets.Contact/Resources/Shaders/sh_PolygonShader.fs
+++ osu.Game.Rulesets.Contact/Resources/Shaders/sh_PolygonShader.fs
@@ -1,8 +1,30 @@
 #define HIGH_PRECISION_VERTEX
 
 #include "sh_Utils.h"
+#include "sh_Masking.h"
 
-void main(){
+uniform vec2 m_CenterPos;
+varying highp vec2 v_TexCoord;
+
+
+float sdCircle( in vec2 p, in float r )
+{
+    return length(p)-r;
+}
+
+
+void main()
+{
+
+    highp vec2 pixelPos = v_TexCoord / (v_TexRect.zw - v_TexRect.xy) - vec2(0.5);
     
-    gl_FragColor = vec4(1,1,1,1);
+    float d = sdCircle(pixelPos,0.1);
+
+    // coloring
+    vec3 col = (d>0.0) ? vec3(0.9,0.6,0.3) : vec3(0.65,0.85,1.0);
+    col *= 1.0 - exp(-6.0*abs(d));
+    col *= 0.8 + 0.2*cos(150.0*d);
+    col = mix( col, vec3(1.0), 1.0-smoothstep(0.0,0.01,abs(d)) );
+    
+    gl_FragColor = vec4(col,1.0);
 }
