d5f8197725e060e7f06551f99227c4da236cff19
mk56-spn <darioheadleysilva@gmail.com>
2023-03-10 10:07:52 +0100

    Move ContactBorder.cs to its own file

---
+++ osu.Game.Rulesets.Contact/Objects/Drawables/ContactBorder.cs
@@ -0,0 +1,37 @@
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Colour;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
+using osu.Game.Rulesets.Contact.UI;
+using osuTK;
+
+namespace osu.Game.Rulesets.Contact.Objects.Drawables;
+
+public partial class ContactBorder : Container
+{
+    private const int border_thickness = 5;
+
+    public ContactBorder()
+    {
+        Depth = float.MaxValue;
+        Colour = Colour4.White;
+        Anchor = Anchor.Centre;
+        Origin = Anchor.Centre;
+        Masking = true;
+        MaskingSmoothness = 0;
+        BorderColour = Colour4.Red;
+        BorderThickness = border_thickness;
+        Size = new Vector2(ContactPlayfield.SIZE + border_thickness * 2);
+        Child = new Box
+        {
+            RelativeSizeAxes = Axes.Both,
+            Colour = new ColourInfo
+            {
+                TopLeft = Colour4.YellowGreen,
+                TopRight = Colour4.Crimson,
+                BottomLeft = Colour4.Aquamarine.Darken(0.3f),
+                BottomRight = Colour4.MediumPurple,
+            }
+        };
+    }
+}
--- osu.Game.Rulesets.Contact/UI/ContactPlayfield.cs
+++ osu.Game.Rulesets.Contact/UI/ContactPlayfield.cs
@@ -1,21 +1,19 @@
 ﻿using osu.Framework.Allocation;
 using osu.Framework.Graphics;
-using osu.Framework.Graphics.Colour;
 using osu.Framework.Graphics.Containers;
-using osu.Framework.Graphics.Shapes;
 using osu.Framework.Utils;
 using osu.Game.Rulesets.Contact.Objects.Controller;
+using osu.Game.Rulesets.Contact.Objects.Drawables;
 using osu.Game.Rulesets.Contact.Scenes;
 using osu.Game.Rulesets.UI;
 using osuTK;
-using osu.Game.Rulesets.Contact.Objects.Shapes;
 
 namespace osu.Game.Rulesets.Contact.UI;
 
 [Cached]
 public partial class ContactPlayfield : Playfield
 {
-    public const int SIZE = 500000;
+    public const int SIZE = 50000;
     private readonly Container viewPort;
 
     private ControllerArea controllerArea { get; }
@@ -30,19 +28,12 @@ public partial class ContactPlayfield : Playfield
         };
         viewPort.AddRange(new Drawable[]
         {
-            new Border(),
+            new ContactBorder(),
             new Scene(),
             controllerArea = new ControllerArea(),
         });
 
         AddInternal(viewPort);
-
-        AddInternal(new TestCirc
-        {
-            Anchor = Anchor.Centre,
-            Origin = Anchor.Centre,
-            Size = new Vector2(20),
-        });
     }
 
     protected override void Update()
@@ -63,34 +54,5 @@ public partial class ContactPlayfield : Playfield
         float y = (float)Interpolation.DampContinuously(drawable.Y, destination.Y, dampLength, Clock.ElapsedFrameTime);
 
         drawable.Position = new Vector2(x, y);
-    }
-
-    private partial class Border : Container
-    {
-        private const int border_thickness = 5;
-
-        public Border()
-        {
-            Depth = float.MaxValue;
-            Colour = Colour4.White;
-            Anchor = Anchor.Centre;
-            Origin = Anchor.Centre;
-            Masking = true;
-            MaskingSmoothness = 0;
-            BorderColour = Colour4.Red;
-            BorderThickness = border_thickness;
-            Size = new Vector2(SIZE + border_thickness * 2);
-            Child = new Box
-            {
-                RelativeSizeAxes = Axes.Both,
-                Colour = new ColourInfo
-                {
-                    TopLeft = Colour4.YellowGreen,
-                    TopRight = Colour4.Crimson,
-                    BottomLeft = Colour4.Aquamarine.Darken(0.3f),
-                    BottomRight = Colour4.MediumPurple,
-                }
-            };
-        }
     }
 }
